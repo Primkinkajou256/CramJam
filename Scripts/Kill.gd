42ac9da1fb9ba5b87fd274bc423416b025559ac0
Primkinkajou256 <s-joshua.ford@lwtech.edu>
2023-07-10 22:01:36 -0700

    Kill Added

--- Scripts/Kill.gd
+++ Scripts/Kill.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-@export var boost_speed = 20
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,7 +9,4 @@ func _ready():
 func _process(delta):
 	for body in get_overlapping_bodies():
 		if body is Rocket:
-			# "rotation" is boost pad's direction
-			body.linear_velocity += -Vector2(0, 1).rotated(body.rotation) * boost_speed
-		if body is Ball:
-			body.linear_velocity += -Vector2(0, 1).rotated(rotation) * boost_speed
+			SceneManager.reset_level()
