cb0cb815e5a7cf40efb2256f084e0a762b5283a1
Primkinkajou256 <s-joshua.ford@lwtech.edu>
2023-07-10 23:37:23 -0700

    Title Screen and also Level 2 Restart Text

---
+++ Scripts/TitleScreen.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var game_started = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Forward") && !game_started:
+		%"Start Game Anim".play("Start")
+		game_started = true
+
+
+func start_game():
+	SceneManager.next_scene()
+
